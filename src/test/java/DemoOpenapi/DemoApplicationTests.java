97011bc6167afd6fa73c7e0314bb69528ecce5a2
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-12-23 22:38:24 +0700

    Setup: init project

---
+++ src/test/java/DemoOpenapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package DemoOpenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
